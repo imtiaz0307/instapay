1dabe316fb20d1d0aa072ef1465d5dc0f6fa8812
Muhammad Imtiaz Hussain <108749606+imtiaz0307@users.noreply.github.com>
2023-11-06 15:48:33 +0500

    Fix the infinite load issue.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,11 +7,11 @@ import white_lg from "../assets/logo.svg"
 
 const Loader = ({ loading }) => {
     return (
-        <div className={`${styles.container} ${!loading && styles.loaded_container}`}>
-            <img className={`${styles.white_lg} ${!loading && styles.loaded_lg}`} src={white_lg} alt="Instapay" />
-            <img className={`${styles.e_636} ${!loading && styles.loaded_636}`} src={e_636} />
-            <img className={`${styles.e_499_2} ${!loading && styles.loaded_499}`} src={e_499_2} />
-            <img className={`${styles.e_499} ${!loading && styles.loaded_499}`} src={e_499} />
+        <div className={`${styles.container} ${!loading ? styles.loaded_container : ""}`}>
+            <img className={`${styles.white_lg} ${!loading ? styles.loaded_lg : ""}`} src={white_lg} alt="Instapay" />
+            <img className={`${styles.e_636} ${!loading ? styles.loaded_636 : ""}`} src={e_636} />
+            <img className={`${styles.e_499_2} ${!loading ? styles.loaded_499 : ""}`} src={e_499_2} />
+            <img className={`${styles.e_499} ${!loading ? styles.loaded_499 : ""}`} src={e_499} />
         </div>
     )
 }
