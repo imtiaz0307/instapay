85d95f1787f6e1d7d2168c596b4fa09c60bc55ad
Muhammad Imtiaz Hussain <108749606+imtiaz0307@users.noreply.github.com>
2023-11-06 15:22:30 +0500

    Update ParticlesBg.jsx

--- src/components/ParticlesBg.jsx
+++ src/components/ParticlesBg.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import sect_bg from "../assets/sec_bg.mp4"
 import sec_bg from "../assets/sect_bg.png"
 
-const ParticlesBg = () => {
+const ParticlesBg = ({borderRadius = "0px"}) => {
     return (
         <>
-            <video src={sect_bg} autoPlay={true} loop={true} muted={true} controls={false} style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", zIndex: 9, mixBlendMode: "difference", pointerEvents: "none", opacity: .3 }}></video>
+            <video src={sect_bg} autoPlay={true} loop={true} muted={true} controls={false} style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", zIndex: 9, mixBlendMode: "difference", pointerEvents: "none", opacity: .3,borderRadius,transition: "1.5s" }}></video>
             <img src={sec_bg} alt="bg" style={{
                 height: "100%",
                 width: "100%",
@@ -13,7 +13,9 @@ const ParticlesBg = () => {
                 position: "absolute",
                 top: "0",
                 left: "0",
-                zIndex: 1
+                zIndex: 1,
+                borderRadius,
+                transition: "1.5s"
             }} />
         </>
     )
