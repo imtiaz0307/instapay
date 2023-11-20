821858d990e330edf17e5b6c8ece6e69e4384e11
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2023-11-20 23:58:16 +0500

    contact us page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import Home from './components/Home'
-import FeatureRow from './components/FeatureRow'
-import { featuresData } from './data/FeaturesData'
-import FeatureTwo from './components/FeatureTwo'
-import Summarized from './components/Summarized'
-import AnimatedSection from './components/AnimatedSection'
-import Loader from './components/Loader'
-import { useEffect, useState } from 'react'
-import FooterNew from './components/FooterNew'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Faq from './Pages/Faq'
-import PrivacyPolicy from './Pages/PrivacyPolicy'
+import Home from "./components/Home";
+import FeatureRow from "./components/FeatureRow";
+import { featuresData } from "./data/FeaturesData";
+import FeatureTwo from "./components/FeatureTwo";
+import Summarized from "./components/Summarized";
+import AnimatedSection from "./components/AnimatedSection";
+import Loader from "./components/Loader";
+import { useEffect, useState } from "react";
+import FooterNew from "./components/FooterNew";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Faq from "./Pages/Faq";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import ContactUs from "./Pages/ContactUs";
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const toggleLoad = () => {
-      console.log("loaded")
+      console.log("loaded");
       setLoading(false);
-    }
+    };
 
     window.addEventListener("load", toggleLoad);
 
@@ -34,53 +35,55 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    (
-      async () => {
-        const LocomotiveScroll = (await import('locomotive-scroll')).default
-        const locomotiveScroll = new LocomotiveScroll();
-      }
-    )()
-  }, [])
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
 
   return (
     <main>
       <Loader loading={loading} />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={
-            <>
-              <Home />
-              <AnimatedSection />
-              <FeatureRow
-                key={featuresData[0].count}
-                count={featuresData[0].count}
-                head={featuresData[0].head}
-                text={featuresData[0].text}
-                img={featuresData[0].img}
-                styles={featuresData[0].styles}
-              />
-              <FeatureTwo />
-              {featuresData.slice(1).map(item => (
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <AnimatedSection />
                 <FeatureRow
-                  key={item.count}
-                  count={item.count}
-                  head={item.head}
-                  text={item.text}
-                  img={item.img}
-                  styles={item.styles}
+                  key={featuresData[0].count}
+                  count={featuresData[0].count}
+                  head={featuresData[0].head}
+                  text={featuresData[0].text}
+                  img={featuresData[0].img}
+                  styles={featuresData[0].styles}
                 />
-              ))}
-              <Summarized />
-              {/* <Footer /> */}
-            </>
-          } />
-          <Route path='/faq' element={<Faq />} />
-          <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+                <FeatureTwo />
+                {featuresData.slice(1).map((item) => (
+                  <FeatureRow
+                    key={item.count}
+                    count={item.count}
+                    head={item.head}
+                    text={item.text}
+                    img={item.img}
+                    styles={item.styles}
+                  />
+                ))}
+                <Summarized />
+                {/* <Footer /> */}
+              </>
+            }
+          />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
         <FooterNew />
       </BrowserRouter>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/FooterNew.jsx
+++ src/components/FooterNew.jsx
@@ -1,107 +1,116 @@
-import React from 'react';
-import { BiLogoFacebook, BiLogoInstagram, BiLogoLinkedin, BiLogoWhatsapp, BiLogoYoutube } from "react-icons/bi"
-import styles from './styles/FooterNew.module.css';
-import logo from "../assets/logo.svg"
-import footer_right from "../assets/footer_right.png"
-import contact_us from "../assets/contact.svg"
-import { FaXTwitter } from "react-icons/fa6"
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  BiLogoFacebook,
+  BiLogoInstagram,
+  BiLogoLinkedin,
+  BiLogoWhatsapp,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import styles from "./styles/FooterNew.module.css";
+import logo from "../assets/logo.svg";
+import footer_right from "../assets/footer_right.png";
+import contact_us from "../assets/contact.svg";
+import { FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const FooterNew = () => {
-    const links = [
-        {
-            link: "#",
-            icon: <BiLogoInstagram color='black' size={20} />
-        },
-        {
-            link: "#",
-            icon: <BiLogoLinkedin color='black' size={20} />
-        },
-        {
-            link: "#",
-            icon: <BiLogoFacebook color='black' size={20} />
-        },
-        {
-            link: "#",
-            icon: <BiLogoWhatsapp color='black' size={20} />
-        },
-        {
-            link: "#",
-            icon: <BiLogoYoutube color='black' size={20} />
-        },
-        {
-            link: "#",
-            icon: <FaXTwitter color='black' size={20} />
+  const links = [
+    {
+      link: "#",
+      icon: <BiLogoInstagram color="black" size={20} />,
+    },
+    {
+      link: "#",
+      icon: <BiLogoLinkedin color="black" size={20} />,
+    },
+    {
+      link: "#",
+      icon: <BiLogoFacebook color="black" size={20} />,
+    },
+    {
+      link: "#",
+      icon: <BiLogoWhatsapp color="black" size={20} />,
+    },
+    {
+      link: "#",
+      icon: <BiLogoYoutube color="black" size={20} />,
+    },
+    {
+      link: "#",
+      icon: <FaXTwitter color="black" size={20} />,
+    },
+  ];
+  const linksData = [
+    {
+      name: "LEGAL & REGULATORY",
+      link: "/",
+    },
+    {
+      name: "Terms and Conditions",
+      link: "/",
+    },
+    {
+      name: "AFFILIATION CERTIFICATE",
+      link: "/",
+    },
+    {
+      name: "PROHIBITED COUNTRIES",
+      link: "/",
+    },
+    {
+      name: "API DOCUMENTATION",
+      link: "/",
+    },
+    {
+      name: "PRIVACY POLICY",
+      link: "/privacy-policy",
+    },
+    {
+      name: "FAQ",
+      link: "/faq",
+    },
+  ];
 
-        },
-    ]
-    const linksData = [
-        {
-            name: "LEGAL & REGULATORY",
-            link: "/"
-        },
-        {
-            name: "Terms and Conditions",
-            link: "/"
-        },
-        {
-            name: "AFFILIATION CERTIFICATE",
-            link: "/"
-        },
-        {
-            name: "PROHIBITED COUNTRIES",
-            link: "/"
-        },
-        {
-            name: "API DOCUMENTATION",
-            link: "/"
-        },
-        {
-            name: "PRIVACY POLICY",
-            link: "/privacy-policy"
-        },
-        {
-            name: "FAQ",
-            link: "/faq"
-        },
-    ]
-
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.wrapper}>
-                <div className={styles.left}>
-                    <div className={styles.top}>
-                        <div className={styles.about}>
-                            <img src={logo} alt="Instapay" />
-                            <div className={styles.socials}>
-                                {
-                                    links.map((link, index) => (
-                                        <a key={index} href={link.link}>{link.icon}</a>
-                                    ))
-                                }
-                            </div>
-                            <span>® 2023 All rights reserved.</span>
-                        </div>
-                        <div className={styles.links}>
-                            {
-                                linksData.map(it => (
-                                    <Link to={it.link}>{it.name.toUpperCase()}</Link>
-                                ))
-                            }
-                        </div>
-                        <div className={styles.contact_us}>
-                            {/* <h3>CONTACT US</h3> */}
-                            <img src={contact_us} alt="contact us" />
-                            <p>Interested in diving deeper? Reach out to us. In case of genuine interest, we'll furnish you with all the necessary details, ensuring a transparent</p>
-                        </div>
-                    </div>
-                </div>
-                {/* <div className={styles.right}>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <div className={styles.top}>
+            <div className={styles.about}>
+              <img src={logo} alt="Instapay" />
+              <div className={styles.socials}>
+                {links.map((link, index) => (
+                  <a key={index} href={link.link}>
+                    {link.icon}
+                  </a>
+                ))}
+              </div>
+              <span>® 2023 All rights reserved.</span>
+            </div>
+            <div className={styles.links}>
+              {linksData.map((it) => (
+                <Link to={it.link}>{it.name.toUpperCase()}</Link>
+              ))}
+            </div>
+            <div className={styles.contact_us}>
+              {/* <h3>CONTACT US</h3> */}
+              <Link to="/contact-us">
+                <img src={contact_us} alt="contact us" />
+              </Link>
+              <p>
+                Interested in diving deeper? Reach out to us. In case of genuine
+                interest, we'll furnish you with all the necessary details,
+                ensuring a transparent
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <div className={styles.right}>
                     <img src={footer_right} alt="instapay" />
                 </div> */}
-            </div>
-        </footer>
-    );
+      </div>
+    </footer>
+  );
 };
 
 export default FooterNew;
