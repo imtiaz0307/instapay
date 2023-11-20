346be75deb8650a015b4bf5b7711706a6e24d67b
Talha Iqbal <talhaiqbal466@gmail.com>
2023-11-20 23:58:59 +0500

    pull

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ const app = express()
 app.use(compression())
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/build'));
-// app.use(express.static(__dirname + '/build/sm-web-booking'));
+app.use(express.static('/build'));
+// app.use(express.static('/build/sm-web-booking'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join('/build/index.html'));
 });
 
 const port = process.env.PORT || '3000';
